1864954b03fe1d88edba5fa82139ac1d95fb3a19
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-07-24 21:32:27 -0300

    Criando micros servicos, Eureka Server e Client

---
+++ eurekaserver/src/test/java/com/giovanni/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovanni.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
