2e9a164c93a8a713e68aa18eaac02845e8548a8c
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-07-25 22:59:47 -0300

    Criando o gateway da aplicação para fazer o balanceamento de carga

--- msclientes/src/main/java/com/giovanni/msclientes/application/ClientesResource.java
+++ msclientes/src/main/java/com/giovanni/msclientes/application/ClientesResource.java
@@ -3,6 +3,7 @@ package com.giovanni.msclientes.application;
 import com.giovanni.msclientes.application.representation.ClienteSaveRequest;
 import com.giovanni.msclientes.domain.Cliente;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -13,6 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("clientes")
 @RequiredArgsConstructor
+@Slf4j
 public class ClientesResource {
 
     private final ClienteService service;
@@ -20,6 +22,7 @@ public class ClientesResource {
 
     @GetMapping
     public String status(){
+        log.info("Obtendo o status do microservice de clientes");
         return "ok";
     }
 
