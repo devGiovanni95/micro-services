81fbec16dc5ee3c5be9902ec6362726d28f9830a
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-07-27 20:41:22 -0300

    Finalizando a criacao do microservico de Cartoes

--- mscartoes/src/main/java/com/giovanni/mscartoes/application/CartoesResource.java
+++ mscartoes/src/main/java/com/giovanni/mscartoes/application/CartoesResource.java
@@ -1,18 +1,24 @@
 package com.giovanni.mscartoes.application;
 
 import com.giovanni.mscartoes.application.representation.CartaoSaveRequest;
+import com.giovanni.mscartoes.application.representation.CartoesPorClienteResponse;
 import com.giovanni.mscartoes.domain.Cartao;
+import com.giovanni.mscartoes.domain.ClienteCartao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("cartoes")
 @RequiredArgsConstructor
 public class CartoesResource {
 
-    private final CartaoService service;
+    private final CartaoService cartaoService;
+    private final ClienteCartaoService clienteCartaoService;
     @GetMapping
     public String status(){
         return "ok";
@@ -21,8 +27,25 @@ public class CartoesResource {
     @PostMapping
     public ResponseEntity cadastrar(@RequestBody CartaoSaveRequest request){
         Cartao cartao = request.toModel();
-        service.save(cartao);
+        cartaoService.save(cartao);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    //  /cartoes?renda=5000
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<Cartao>> getCartoesTendaAte(@RequestParam("renda") Long renda){
+        List<Cartao> list = cartaoService.getCartoesRendaMenorIgual(renda);
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity<List<CartoesPorClienteResponse>> getCartoesByCliente(@RequestParam("cpf") String cpf){
+        List<ClienteCartao> lista = clienteCartaoService.listCartoesByCpf(cpf);
+        List<CartoesPorClienteResponse> resultList = lista.stream()
+                .map(CartoesPorClienteResponse::fromModel)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(resultList);
+
     }
 
 }
---
+++ mscartoes/src/main/java/com/giovanni/mscartoes/application/ClienteCartaoService.java
@@ -0,0 +1,19 @@
+package com.giovanni.mscartoes.application;
+
+import com.giovanni.mscartoes.domain.ClienteCartao;
+import com.giovanni.mscartoes.infra.ClienteCartaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteCartaoService {
+
+    private final ClienteCartaoRepository repository;
+
+    public List<ClienteCartao> listCartoesByCpf(String cpf){
+        return repository.findByCpf(cpf);
+    }
+}
